0fbbd2949095aac62c9c6c06f24dfc22e4932a45
inlinebool <jiangkairong@email.arizona.edu>
2020-08-11 03:28:55 -0700

    changed selected highlight to grey

--- src/components/Sentence.vue
+++ src/components/Sentence.vue
@@ -1,36 +1,43 @@
 <template>
   <v-hover v-slot:default="{ hover }">
-    <v-card
-      :elevation="hover ? 2 : 0"
-      class="mx-3"
-      :class="{ 'on-hover': hover , 'blue lighten-4 font-weight-medium': selected, 'purple lighten-5': hit}"
-      style="cursor:pointer;user-select:none"
-      width="100%"
-      v-if="enabled"
-    >
-      <span class="mr-2">
-        <span @click="onClickFact">
-          <span>[ {{ sentence[0] }} ]</span>
-          <span>{{ sentence[1] }}</span>
-        </span>
+    <v-container>
+      <v-row>
         <v-btn
           v-if="sentence[2]"
-          class="font-italic ma-auto"
+          class="font-italic text-decoration-underline"
           @click="onClickTitle"
           text
           small
         >({{ sentence[2] }})</v-btn>
-        <v-expand-transition v-if="sentence[2]">
-          <v-card v-show="expand" width="70%" class="mx-auto">
-            <v-card-text>
-              <v-row v-for="sentence in paragraph[1]" :key="sentence[0]" class="my-2">
-                <Sentence :sentence="sentence" v-model="value" :enabled="true" />
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-expand-transition>
-      </span>
-    </v-card>
+      </v-row>
+      <v-row>
+        <v-card
+          :elevation="hover ? 2 : 0"
+          class="mx-3"
+          :class="{ 'on-hover': hover , 'grey lighten-3 font-weight-medium': selected, 'purple lighten-5': hit}"
+          style="cursor:pointer;user-select:none"
+          width="100%"
+          v-if="enabled"
+          @click.native="onClickFact"
+        >
+          <span class="mr-2">
+            <span>
+              <span>[ {{ sentence[0] }} ]</span>
+              <span>{{ sentence[1] }}</span>
+            </span>
+            <v-expand-transition v-if="sentence[2]">
+              <v-card v-show="expand" width="70%" class="mx-auto">
+                <v-card-text>
+                  <v-row v-for="sentence in paragraph[1]" :key="sentence[0]" class="my-2">
+                    <Sentence :sentence="sentence" v-model="value" :enabled="true" />
+                  </v-row>
+                </v-card-text>
+              </v-card>
+            </v-expand-transition>
+          </span>
+        </v-card>
+      </v-row>
+    </v-container>
   </v-hover>
 </template>
 
